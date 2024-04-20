e28445b70255489871569fbb47da4945f27f6d22
gr1ntch <odewumimighty@gmail.com>
2024-04-20 05:33:03 +0100

    Started working on the Convo screen UI

--- src/pages/mainUI/chat/NewUsers.jsx
+++ src/pages/mainUI/chat/NewUsers.jsx
@@ -34,13 +34,32 @@ export default function NewUsers({ userId }) {
 
   return (
     <div>
-      <h2>New Users</h2>
-      <ul>
-        {newUsers.map((appUser) => (
-          <li key={appUser.id}>
-            <Link to={`/chat?senderId=${userId}&recipientId=${appUser.uid}`}>--{appUser.name}</Link>
-          </li>
-        ))}
+      <h2 className="font-bold font-inter text-sm mb-3">New Users</h2>
+      <ul className="flex overflow-x-scroll gap-4">
+        {newUsers.map((appUser) => {
+          const username = appUser.name;
+          const nameArray = username.split("");
+          const userImg = (nameArray[0] + nameArray[1]).toUpperCase();
+
+          return (
+            <li key={appUser.id}  className="hover:bg-gray-100 transition-all p-2">
+              <Link 
+                to={`/chat?senderId=${userId}&recipientId=${appUser.uid}`}
+               
+              >
+                <div className="flex flex-col items-center ">
+                  <img 
+                    src={appUser.avatar} 
+                    alt="user avatar" 
+                    className="ring-2 rounded-full w-12 h-12"
+                  />
+                  {/* <div className="ring-2 rounded-[100%] text-center fl ex justify-cent er font-bold ">{userImg}</div> */}
+                  <span className="mt-2">{appUser.name}</span>
+                </div>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </div>
   );

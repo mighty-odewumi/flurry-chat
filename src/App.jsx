c47e89b3319ac379182dc6de4e7015e1b001ee52
gr1ntch <odewumimighty@gmail.com>
2024-08-04 04:19:41 +0100

    fix: Fixed the Conversations listing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,12 @@ import {
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
-import { getAuth, connectAuthEmulator, onAuthStateChanged } from "firebase/auth";
-import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 // Firebase Config
 import { firebaseConfig } from "./config";
 
 // import {  requestPopup } from "./utils/messaging_get_token";
-import { useEffect, useState } from "react";
-
 // Import auth components
 // import SendEmailVerification from "./auth/SendEmailVerification";
 // import GetUserProfile from "./auth/GetUserProfile";
@@ -37,10 +34,9 @@ import SignUpPage, {
   loader as signUpLoginLoader 
 } from "./pages/authPages/SignUpPage";
 
-import { RequireAuth, } from "./auth/requireAuth";
 import SignOut from "./auth/SignOut";
 import DirectChat, { action as chatAction } from "./pages/mainUI/chat/DirectChat";
-import ConversationsList from "./pages/mainUI/chat/ConversationsList";
+import Conversations from "./pages/mainUI/chat/Conversations";
 import AuthProvider, { useAuth } from "./auth/AuthContext";
 import Profile from "./pages/mainUI/userProfile/Profile";
 // import { saveMessagingDeviceToken } from "./firebase/messaging";
@@ -56,38 +52,12 @@ export default function App() {
   const app = initializeApp(firebaseConfig);
   // const analytics = getAnalytics(app);
 
-
   const messaging = getMessaging(app);
 
   const db = getFirestore();
 
   const { user } = useAuth();
   
-
-  // localStorage.clear();
-
-  // saveMessagingDeviceToken();
-
-  // admin.initializeApp({
-  //   credential: admin.credential.applicationDefault(),
-  // });
-
-  // useEffect(() => {
-  //   const auth = getAuth(app);
-  //   const observer = onAuthStateChanged(auth, user => {
-  //     if (user) {
-  //       setUserId(user.uid);
-  //     } else {
-  //       setUserId(null);
-  //     }
-  //   });
-
-
-  //   return () => observer();
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-
   const router = createBrowserRouter(createRoutesFromElements(
     <>
       <Route 
@@ -113,7 +83,7 @@ export default function App() {
         path="/conversations"
         element={
           <>
-            <ConversationsList />
+            <Conversations userId={user?.uid}/>
             {/* <SignOut /> */}
           </>
         }

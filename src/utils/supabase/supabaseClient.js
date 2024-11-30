fb7aaa61d0aa59d899d482c8e3d1e553bbb0eef3
gr1ntch <odewumimighty@gmail.com>
2024-12-01 00:37:17 +0100

    Stored Supabase bucket to .env

--- src/utils/supabase/supabaseClient.js
+++ src/utils/supabase/supabaseClient.js
@@ -1,14 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 // import { getFirebaseToken } from "../getFirebaseToken";
 
-const supabaseUrl = "https://osnkgvzajelaqxdorsmk.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_BUCKET;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
-// const token = await getFirebaseToken();
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, 
-  // {
-  //   headers: {
-  //     Authorization: `Bearer ${token}`
-  //   }
-  // }
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
